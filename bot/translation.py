da29ede8c29f7c5cef2154836e08a7bc1ac41d7a
njanunni777 <85037210+njanunni777@users.noreply.github.com>
2021-06-13 13:45:06 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -42,11 +42,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/G9037944937">MMM5</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/G9037944937">5555 M...</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
+<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/njanunni777/AutoFilterBotV4">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
